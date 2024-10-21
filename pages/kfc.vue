79169852ad3ed74600a9c1a3a5c4ea1276b5502c
Shieru-ktc <ktc24a31g0006@edu.kyoto-tech.ac.jp>
2024-10-21 14:26:49 +0900

    Move codepen KFC page to project

---
+++ pages/kfc.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="#">C○○kpad</a></li>
+      <li class="breadcrumb-item"><a href="#">「フライドチキン」人気ランキング</a></li>
+      <li class="breadcrumb-item active">美味しさNo.１！KFC風フライドチキン</li>
+    </ol>
+  </div>
+
+  <div class="container">
+    <h1 class="display-3 border-bottom border-3">美味しさNo.１！KFC風フライドチキン</h1>
+    <p class="text-body-secondary"><abbr
+        title="ケンタッキー・フライド・チキン">KFC</abbr>人気レシピ1位！数あるKFCレシピ─どれも何か一味足りない気が。試行錯誤の末の主人も絶賛☆再現率の高いレシピです☆</p>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-4 card">
+        <div class="card-body">
+          <h2 class="card-title">材料</h2>
+          <div class="mx-3">
+            <ul>
+              <li>鶏肉(お好きな部位)　２〜３枚位</li>
+              <li>☆牛乳　1/2カップ</li>
+              <li>☆卵　１個</li>
+              <li>☆にんにくチューブ　２センチ位</li>
+              <li>★薄力粉　１カップ</li>
+              <li>★オールスパイス　小さじ１弱</li>
+              <li>★顆粒コンソメ　大さじ1/2</li>
+              <li>★塩　大さじ1/2</li>
+              <li class="text-muted">あれば★パプリカパウダー　小さじ１</li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+      <div class="col-12 col-md-4 border-2 border rounded p-3">
+        <h2>作り方</h2>
+        <div>
+          <ol class="mx-3">
+            <li>☆をボウルに混ぜ合わせておく</li>
+            <li>ボウルに★を全て混ぜ合わせておく。</li>
+            <li>肉の厚みがある部分を切り開き、<mark>厚みを一定にする。</mark>鶏肉１枚を２～３等分の大きめに切る。</li>
+            <li>１のボウルに鶏肉を浸す。(時間があれば20分以上浸す。)</li>
+            <li>２のボウルに鶏肉を入れて粉類を<mark>まんべんなく</mark>まぶし、別の容器に並べていく。<small
+                class="text-body-secondary">粉が余るようでしたら、二度付けをお勧めします！</small></li>
+            <li>卵液→粉→卵液→粉と二度付けする事で、カリっと感がアップ＋味もしっかり付きます！写真の我が家のは二度付けしています。</li>
+            <li>油を160度に温める。(温めるのはこのタイミングで大丈夫です。待ってる間に衣が鶏肉になじみます。)</li>
+            <li>160度で５分以上、様子を見ながら、火が通ったら、一度上げて、今度は180～200度でカリっと仕上げ、完成です☆</li>
+            <li>鶏肉の1枚はサイズにばらつきがある為、2～3枚という記載に変更しました。</li>
+          </ol>
+        </div>
+      </div>
+      <div class="col-12 col-md-4 border-2 border rounded p-3">
+        <h2>コツ・ポイント</h2>
+        <div class="mx-3">
+          <p>揚げる際鍋底での焦げ防止に衣が軽く揚がるまで菜箸等で時々浮かせて下さい</p>
+          <p>好みで粉類に粗びき黒胡椒小さじ1/2を足しても◎</p>
+          <p>にんにくチューブ２センチはガーリックパウダー小さじ１に変えて粉類と混ぜても美味しいです。</p>
+          <p>パプリカパウダーは無くても可</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container mt-3">
+    <h2 class="text-center">このレシピの生い立ち</h2>
+    <p>今まで作ったKFCレシピは、卵液や粉もかなり余るし、何よりも</p>
+    <p>何かひと味足りない……と思って、やっとKFC味にたどり着きました！</p>
+    <p>どの家庭にもあるような材料で☆</p>
+    <p>牛乳と卵液、粉類も必要量のみになっているので無駄が無く経済的です☆</p>
+  </div>
+</template>
